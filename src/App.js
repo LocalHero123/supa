69884cefcc985de969e980740ed634bcb86762b5
LocalHero <arsen.abdulaev543@gmail.com>
2022-09-18 22:43:54 +0300

    insert and get data supabase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useSubscription, useRealtime } from 'react-supabase';
 import './App.css';
-import { supabase } from './client';
-import Loader from './components/Loader';
+import Chat from './components/Chat';
 
 export default function App() {
-   const [value, setValue] = useState('')
-
-   const [{ data: messages, error, fetching }, reexecute] = useRealtime('messages')
-
-   async function createMessage(e) {
-      e.preventDefault()
-      const { data, error } = await supabase.from('messages').insert({ message: value })
-      setValue('')
-   }
-
    return (
-      <div className="App">
-         <form action="">
-            <input value={value} onChange={(e) => setValue(e.target.value)} />
-            <button onClick={createMessage}>Send</button>
-         </form>
-
-         {fetching && <Loader />}
-         <div>{messages && messages.map(i => <div key={i.id}>{i.message}</div>)}</div>
+      <div className='App'>
+         <Chat />
       </div>
    );
 }
---
+++ src/components/Chat.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useRealtime } from 'react-supabase';
+import './../App.css';
+import { supabase } from './../client';
+import Loader from './Loader';
+
+export default function Chat() {
+   const [value, setValue] = useState('')
+
+   const [{ data: messages, error, fetching }, reexecute] = useRealtime('messages')
+
+   async function createMessage(e) {
+      e.preventDefault()
+      if (value !== '') {
+         const { data, error } = await supabase.from('messages').insert({ message: value })
+         setValue('')
+      }
+   }
+   window.scrollTo(0, 999999)
+
+
+   return (
+      <div className='Chat'>
+         <div className="container">
+            <div className="messages-area">
+               {fetching && <Loader />}
+               {messages && messages.map(i =>
+                  <div className='message' key={i.id}>
+                     <img className='message__photo' src='https://sun2.6789.userapi.com/s/v1/ig2/ko_i9YzeASmx2JlChnicHp52BzfOgcdO1n_5PRtp3zkczstejwAH22A59kykmzObREbf58sd81es-gy5O3hzN6su.jpg?size=50x50&quality=96&crop=0,2,402,402&ava=1' alt="Ава" />
+
+                     <div className='message__body'>
+                        <span className="message__name">LocalHero</span>
+
+                        <span className='date'>
+                           <span className="date__time">
+                              {Number(i.date.substr(11, 2)) + 3 + i.date.substr(13, 3)}
+                           </span>
+
+                           <span className="date__day">{i.date.substr(0, 10)}</span>
+                        </span>
+
+                        <div className="message__text">{i.message}</div>
+                     </div>
+                  </div>
+               )}
+            </div>
+         </div>
+
+         <form className='form'>
+            <input
+               className='form__input'
+               type="text"
+               placeholder='Введите ваше сообщение...'
+               value={value} onChange={(e) => setValue(e.target.value)} />
+
+            <button type='submit' className='form__btn' onClick={createMessage}>Отправить</button>
+         </form>
+      </div>
+   )
+}
